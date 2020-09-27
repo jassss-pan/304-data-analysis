302da7fbc64a4b03f327e4b07b4d789436cdd7bb
jassss-pan <minhui.pan@mail.utoronto.ca>
2020-09-27 18:47:55 +0000

    first

---
+++ 2.R
@@ -0,0 +1,20 @@
+install.packages("tidyverse")
+
+install.packages("blogdown")
+
+library(blogdown)
+
+blogdown::new_site(
+  dir = ".",
+  install_hugo = TRUE,
+  format = "toml",
+  sample = TRUE,
+  theme = "yihui/hugo-lithium",
+  hostname = "github.com",
+  theme_example = TRUE,
+  empty_dirs = FALSE,
+  to_yaml = TRUE,
+  serve = interactive()
+)
+
+blogdown::serve_site()
